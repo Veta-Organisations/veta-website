e25ab3d3ac95962667c3b66aeb23a8e3740f4719
MAC <mac@MACs-MacBook-Air.local>
2025-09-17 03:37:13 +0100

    Gradient Update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="fixed top-0 bg-white md:h-[75px] p-6 z-50 w-full">
+    <nav className="absolute top-0 bg-transparent md:h-[75px] p-6 z-50 w-full">
       <div className="flex justify-between items-center mx-auto max-w-6xl">
         <div>
           <img src="/logo.png" className="w-[95px] h-[24px]" alt="Veta Logo" />
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,7 +8,25 @@ import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
-    <main className="">
+    <main
+      className="relative min-h-screen bg-cover bg-no-repeat bg-center overflow-hidden"
+      style={{ backgroundImage: "url('/bg.png')" }}
+    >
+      {/* Watermark logo overlay */}
+      <img
+        src="/footer-logo.svg"
+        alt="Veta watermark top-left"
+        className="absolute top-[100px] left-[-700px] w-[1500px] opacity-3 rotate-[-25deg] pointer-events-none select-none"
+      />
+
+      {/* Bottom-right logo */}
+      <img
+        src="/footer-logo.svg"
+        alt="Veta watermark bottom-right"
+        className="absolute top-[700px] right-[-900px] w-[1500px] opacity-3 rotate-[-25deg] pointer-events-none select-none"
+      />
+
+      {/* Content */}
       <Navbar />
       <Hero />
       <Offer />
